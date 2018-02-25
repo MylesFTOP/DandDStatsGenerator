e1a9a0be6ff4d024bbcf7ef28b9679ae4dd90ec2
MylesFTOP <m.fullen87@gmail.com>
2018-02-25 15:04:03 +0000

    Initial commit

---
+++ DandDStatsGenerator/MainForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DandDStatsGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenerateStats = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GenerateStats
+            // 
+            this.GenerateStats.Location = new System.Drawing.Point(84, 144);
+            this.GenerateStats.Name = "GenerateStats";
+            this.GenerateStats.Size = new System.Drawing.Size(86, 38);
+            this.GenerateStats.TabIndex = 0;
+            this.GenerateStats.Text = "Generate stats";
+            this.GenerateStats.UseVisualStyleBackColor = true;
+            this.GenerateStats.Click += new System.EventHandler(this.GenerateStats_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 393);
+            this.Controls.Add(this.GenerateStats);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateStats;
+    }
+}
---
+++ DandDStatsGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using DandDLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DandDStatsGenerator
+{
+    class Program
+    {
+        // Start with form that displays name
+        // Use libraries to manage instantiation
+        // Random name generator?
+        // Add rows for important stats
+        // Export to JSON and XML?
+
+        private static List<Character> characters = new List<Character>();
+
+        static void Main(string[] args)
+        {
+            SetUpSampleCharacter();
+            GreetAllTheCharacters();
+        }
+
+        private static void SetUpSampleCharacter()
+        {
+            characters.Add(new Character { FirstName = "Bob", LastName = "Smith", Strength = 15 });
+        }
+
+        private static void GreetAllTheCharacters()
+        {
+            foreach (var character in characters)
+            {
+                Console.WriteLine($"Hi, {character.FirstName} {character.LastName}! You have a Strength of {character.Strength}!");
+            }
+        }
+    }
+}
